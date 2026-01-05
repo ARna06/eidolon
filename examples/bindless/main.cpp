5f94200ee714b4a54bb3f949dd79763eea7f7fd3
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2026-01-05 22:36:19 +0530

    update + formatting

---
+++ examples/bindless/main.cpp
@@ -0,0 +1,70 @@
+#include "eidolon/core/core.hpp"
+#include "eidolon/core/logger.hpp"
+#include "eidolon/core/window.hpp"
+#include "math/math.hpp"
+
+#include "eidolon/gfx/base.hpp"
+#include "eidolon/gfx/context.hpp"
+#include "eidolon/gfx/helper.hpp"
+#include "eidolon/gfx/types.hpp"
+#include <vulkan/vulkan_core.h>
+
+struct push_constant_t {
+  VkDeviceAddress result;
+};
+
+int main(int argc, char **argv) {
+  core::ref<core::window_t> window =
+      core::make_ref<core::window_t>("test", 640, 420);
+  core::ref<gfx::context_t> context = core::make_ref<gfx::context_t>(true);
+  gfx::base_t base{window, context};
+
+  gfx::handle_image_t image = gfx::helper::load_image_from_path_instant(
+      *context, base._command_pool,
+      "../../assets/textures/draw-3583548_1280.png", VK_FORMAT_R8G8B8A8_SRGB);
+  gfx::handle_image_view_t image_view =
+      context->create_image_view({.handle_image = image});
+
+  gfx::handle_sampler_t sampler = context->create_sampler({});
+
+  auto bimage = base.new_bindless_image();
+  auto bsampler = base.new_bindless_sampler();
+  eidolon_info("bimage {}", bimage.val);
+  eidolon_info("bsampler {}", bsampler.val);
+
+  base.set_bindless_image(bimage, image_view,
+                          VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+  base.set_bindless_sampler(bsampler, sampler);
+
+  gfx::config_pipeline_layout_t cpl{};
+  cpl.add_descriptor_set_layout(base._bindless_descriptor_set_layout);
+  cpl.add_push_constant(sizeof(push_constant_t), VK_SHADER_STAGE_ALL);
+  gfx::handle_pipeline_layout_t pl = context->create_pipeline_layout(cpl);
+
+  gfx::config_pipeline_t cp{};
+  cp.handle_pipeline_layout = pl;
+  cp.add_shader(gfx::helper::create_slang_shader(
+      *context, "../../assets/shaders/bindless/bindless.slang",
+      gfx::shader_type_t::e_compute));
+  gfx::handle_pipeline_t p = context->create_compute_pipeline(cp);
+
+  gfx::config_buffer_t cb{};
+  cb.vk_size = sizeof(math::vec4);
+  cb.vk_buffer_usage_flags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+  cb.vma_allocation_create_flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT;
+  gfx::handle_buffer_t b = context->create_buffer(cb);
+
+  auto cbuf =
+      gfx::helper::begin_single_use_commandbuffer(*context, base._command_pool);
+  context->cmd_bind_pipeline(cbuf, p);
+  context->cmd_bind_descriptor_sets(cbuf, p, 0,
+                                    {base._bindless_descriptor_set});
+  push_constant_t pc{};
+  pc.result = context->get_buffer_device_address(b);
+  context->cmd_push_constants(cbuf, p, VK_SHADER_STAGE_ALL, 0,
+                              sizeof(push_constant_t), &pc);
+  context->cmd_dispatch(cbuf, 1, 1, 1);
+  gfx::helper::end_single_use_command_buffer(*context, cbuf);
+  math::vec4 *v = (math::vec4 *)context->map_buffer(b);
+  eidolon_info("{}", math::to_string(*v));
+}
