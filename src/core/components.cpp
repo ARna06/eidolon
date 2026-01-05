912d690f7ae07a96cabbb590b3deb74db1a1733c
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2026-01-05 23:05:02 +0530

    update in camera component

---
+++ src/core/components.cpp
@@ -0,0 +1,21 @@
+#include "eidolon/core/components.hpp"
+
+#include "math/math.hpp"
+
+namespace core {
+
+void camera_t::update() {
+  inv_projection = math::inverse(projection);
+  inv_view       = math::inverse(view);
+}
+math::vec3 camera_t::position() const {
+  return {inv_view[3][0], inv_view[3][1], inv_view[3][2]};
+}
+
+math::mat4 transform_t::mat4() const {
+  return math::translate(math::mat4{1.f}, translation) *
+         math::toMat4(math::quat{rotation}) *
+         math::scale(math::mat4{1.f}, scale);
+}
+
+}  // namespace core
